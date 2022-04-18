50f4808578181c0a1ffb5995a9b72422d922cef2
muhammadnurulahsan <mdnurulahsanbdbd@gmail.com>
2022-04-18 13:39:06 +0600

    login page added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,13 +1,81 @@
-import React from 'react';
+import React from "react";
 import "./Login.css";
+import gImg from "../../Img/google.png";
+import gitImg from "./../../Img/github.png";
+import fbImg from "./../../Img/facebook.png";
+import { useNavigate } from "react-router-dom";
+
 
 const Login = () => {
-    return (
-        <div className="custom-margin">
-            <div ></div>
-            <div></div>
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="custom-margin">
+      <div className="bg-img">
+        <div className="login-form-container">
+          <div className="login-form">
+            <h1 className="mt-4">Login</h1>
+            <form>
+              <div className="input-field">
+                <label htmlFor="email">Email</label>
+                <div className="input-wrapper">
+                  <input type="text" name="email" id="email" />
+                </div>
+                {/* {email.error && (
+              <p className='error'>
+                <AiOutlineExclamationCircle /> {email.error}
+              </p>
+            )} */}
+              </div>
+              <div className="input-field">
+                <label htmlFor="password">Password</label>
+                <div className="input-wrapper">
+                  <input
+                    type="password"
+                    // onBlur={handlePassword}
+                    name="password"
+                    id="password"
+                  />
+                </div>
+                {/* {password.error && (
+              <p className='error'>
+                <AiOutlineExclamationCircle /> {password.error}
+              </p>
+            )} */}
+              </div>
+              <button type="submit" className="login-form-submit">
+                Login
+              </button>
+            </form>
+            <div className="horizontal-divider mt-4">
+              <div className="line-left" />
+              <p>or</p>
+              <div className="line-right" />
+            </div>
+            <div className="d-flex justify-content-center my-3">
+              <button className="btn shadow rounded-pill me-4">
+                <img height="45px" width="40px" src={gitImg} alt="facebook-icon" />
+              </button>
+              <button className="btn shadow rounded-pill me-4">
+                <img src={gImg} height="45px" width="40px" alt="google-icon" />
+              </button>
+              <button className="btn shadow rounded-pill me-4">
+                <img height="45px" width="42px" src={fbImg} alt="github-icon" />
+              </button>
+            </div>
+            <p className="redirect">
+              Don't have an account?{" "}
+              <span className="ms-3" onClick={() => navigate("/register")}>
+                Please Register!
+              </span>
+            </p>
+            <div className=""></div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,12 +1,76 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Register.css";
 
 const Register = () => {
-    return (
-        <div className="custom-margin">
-            <h2>this register</h2>
+  const navigate = useNavigate();
+
+  return (
+    <div className="custom-margin">
+      <div className="register-form-container">
+        <div className="register-form">
+          <h1 className="mt-4">Register</h1>
+          <form>
+            <div className="input-field">
+              <label htmlFor="name">Name</label>
+              <div className="input-wrapper">
+                <input type="text" name="name" id="name" placeholder="Enter Your Full Name" />
+              </div>
+              {/* {email.error && (
+                <p className='error'>
+                  <AiOutlineExclamationCircle /> {email.error}
+                </p>
+              )} */}
+            </div>
+            <div className="input-field">
+              <label htmlFor="email">Email</label>
+              <div className="input-wrapper">
+                <input type="text" name="email" id="email" placeholder="Enter Your Email"/>
+              </div>
+              {/* {email.error && (
+                <p className='error'>
+                  <AiOutlineExclamationCircle /> {email.error}
+                </p>
+              )} */}
+            </div>
+            <div className="input-field">
+              <label htmlFor="password">Password</label>
+              <div className="input-wrapper">
+                <input
+                  type="password"
+                  // onBlur={handlePassword}
+                  name="password"
+                  id="password"
+                  placeholder="Enter your password"
+                />
+              </div>
+              {/* {password.error && (
+                <p className='error'>
+                  <AiOutlineExclamationCircle /> {password.error}
+                </p>
+              )} */}
+            </div>
+            <button type="submit" className="register-form-submit">
+              Register
+            </button>
+          </form>
+          <div className="horizontal-divider mt-4">
+            <div className="line-left" />
+            <p>or</p>
+            <div className="line-right" />
+          </div>
+          <p className="redirect">
+            Already have an account?
+            <span className="ms-3" onClick={() => navigate("/login")}>
+              Please Login!
+            </span>
+          </p>
+          <div className=""></div>
         </div>
-    );
+        <div className="register-bg-img"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
